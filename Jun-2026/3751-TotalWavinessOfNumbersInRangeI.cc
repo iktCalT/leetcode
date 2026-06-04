c15ed1c980d1ace760580dac981fd87f73b4c5bb
iktCalT <96046592+iktCalT@users.noreply.github.com>
2026-06-04 23:15:22 +0800

    3751

---
+++ Jun-2026/3751-TotalWavinessOfNumbersInRangeI.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  int totalWaviness(int num1, int num2) {
+    // method 1 brute force 
+    if (num1 > num2) return 0;
+    int cnt = 0;
+    for (int i = num1; i <= num2; ++i) {
+      cnt += countWaviness(i);
+    }
+    
+    return cnt;
+  }
+  
+ private:
+   int countWaviness (int num) {
+     int cnt = 0;
+     string str = to_string(num);
+      for (int i = 1; i < str.size() - 1; ++i) {
+        cnt += (str[i] - str[i - 1]) * (str[i] - str[i + 1]) > 0;
+      }
+      return cnt;
+   }
+};
