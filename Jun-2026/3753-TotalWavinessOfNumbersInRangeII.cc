2a0a46b4bbc1a22793df0587a406718435f72971
iktCalT <zzj610644618@live.com>
2026-06-18 01:44:02 +0800

    3753

--- Jun-2026/3753-TotalWavinessOfNumbersInRangeII.cc
+++ Jun-2026/3753-TotalWavinessOfNumbersInRangeII.cc
@@ -6,17 +6,17 @@
 
 using namespace std;
 
-class Solution {
+class Solution0 {
 public:
   long long totalWaviness(long long num1, long long num2) {
-    // f(x0000, x9999) can be written as 
+    // f(x0000, x9999) can be written as
     // g(x, 3)
     //    first parameter: leading digit (it can be 0)
     //    second parameter: number of following 9s
 
-    // f(1234, 4321)  
+    // f(1234, 4321)
     //    = f(0, 4321) - f(0, 1233)
-    // so, f(0, 4321) 
+    // so, f(0, 4321)
     //    = g(0, 3) + g(1, 3) + g(2, 3) + g(3, 3)   // to 3999
     //    + g(40, 2) + g(41, 2) + g(42, 2)          // to 4299
     //    + g(430, 1) + g(431, 1)                   // to 4319
@@ -31,7 +31,8 @@ private:
     long long cnt = 0;
 
     // Be careful when x == 0, because 083 actually is not a wavy
-    if (x == 0 & n == 0) return 0;
+    if (x == 0 & n == 0)
+      return 0;
     if (x == 0) {
       // e.g. g(0, 3) -> 000 to 999
       for (int i = 0; i <= 9; ++i) {
@@ -46,8 +47,8 @@ private:
     // how many waviness in x
     // e.g. x = 184574 -> 184, 845, 574 -> 3
     string str = to_string(x);
-    long long leading = 0;  // we can delete this variable to make it 
-                            // faster, but this is more readable
+    long long leading = 0; // we can delete this variable to make it
+                           // faster, but this is more readable
     for (int i = 2; i < str.size(); ++i) {
       if ((str[i - 1] - str[i - 2]) * (str[i - 1] - str[i]) > 0) {
         ++leading;
@@ -62,8 +63,8 @@ private:
       long long special = 0;
       if (str[str.size() - 2] > str.back()) {
         // ...75 -> 6, 7, 8, 9 -> 4
-        special = 9 - last; 
-            // if we use str.back(), remember to write (str.back() - '0')
+        special = 9 - last;
+        // if we use str.back(), remember to write (str.back() - '0')
       } else if (str[str.size() - 2] < str.back()) {
         // ...57 -> 0, 1, ..., 6 -> 7
         special = last;
@@ -73,7 +74,7 @@ private:
 
     // extra waviness 2:
     // notice that 184574(5100) need to plus 1 waviness: 451
-    // ....a(bc...) if (b > c), a can be 0, 1, ..., b-1; 
+    // ....a(bc...) if (b > c), a can be 0, 1, ..., b-1;
     // if b < c, a can be b+1, b+2, ..., 9
     if (n >= 2) {
       long long special_2 = 0;
@@ -94,7 +95,7 @@ private:
 
   long long h(long long num) {
     // return totalWaviness(0, num)
-    // e.g. 3876 
+    // e.g. 3876
     //      = g(3876, 0) + g(3875, 0) + ... + g(3870, 0)
     //      + g(386, 1) + g(385, 1) + ... + g(380, 1)
     //      + g(37, 2) + g(36, 2) + ... + g(30, 2)
@@ -107,7 +108,8 @@ private:
       --num;
       // 3876, 386, 37, 2
       for (int _ = 0; _ < ones; ++_) {
-        printf("calling g(%lld, %d), result:(%lld)\n", num, zeros, g(num, zeros));
+        printf("calling g(%lld, %d), result:(%lld)\n", num, zeros,
+               g(num, zeros));
         cnt += g(num, zeros); // g(3875, 0), g(3874, 0), ..., g(3870, 0)
         --num;
       }
@@ -123,14 +125,15 @@ private:
   long long tail(int n) {
     // if n = 4, return the number of waviness from 0000 to 9999
     // leading 0s are included (meaning 0813 is 2: 081 and 813)
-    if (check_table.contains(n)) return check_table[n];
+    if (check_table.contains(n))
+      return check_table[n];
     long long ans = 0;
     if (n == 3) {
       for (int i = 1; i <= 9; ++i) {
         ans += 2 * i * i;
       }
     } else {
-      ans =  tail(n - 1) * 10 + tail(3) * pow(10, n - 3);
+      ans = tail(n - 1) * 10 + tail(3) * pow(10, n - 3);
     }
 
     check_table.insert({n, ans});
@@ -139,6 +142,6 @@ private:
 };
 
 int main() {
-  Solution demo;
+  Solution0 demo;
   std::cout << demo.totalWaviness(1, 98765432109876) << "\n"; // 671319920770693
 }
