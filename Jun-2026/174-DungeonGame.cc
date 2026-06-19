35589812a34d1a5c6d987ba40309f2c98d6053a1
iktCalT <zzj610644618@live.com>
2026-06-19 11:15:43 +0800

    174 new_solution

--- Jun-2026/174-DungeonGame.cc
+++ Jun-2026/174-DungeonGame.cc
@@ -5,17 +5,55 @@
 
 using namespace std;
 
-struct Path {
+class Solution { // 0ms
+public:
+  int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    // dp
+    // 1. Let's create a 2D array dp, where dp[i][j]
+    // represents the min health needed from 
+    // [i][j] to [m][n]
+    // 2. Go reverse order, if we know dp[i+1][j] and 
+    // dp[i][j+1], we can know 
+    // dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1]))
+    //
+    // Why reverse order? If we don't go reverse order, 
+    // dp[i][j] is min hp needed from [0][0] to [i][j].
+    // Then, if we know dp[i-1][j] and dp[i][j-1], then
+    // we also need to know current hp of [i-1][j] and 
+    // [i][j-1] to calculate dp[i][j]
+    // But in reverse order, we don't need that, because 
+    // dungeon[i][j] must be subtracted from dp[i][j], since
+    // it is experienced prior
+
+    int m = dungeon.size();
+    int n = dungeon[0].size();
+
+    vector<vector<int>> dp(m + 1, vector(n + 1, INT_MAX));
+    dp[m][n - 1] = 1;
+    dp[m - 1][n] = 1;
+
+    for (int i = m - 1; i >= 0; --i) {
+      for (int j = n - 1; j >= 0; --j) {
+        dp[i][j] = 
+          max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+      }
+    }
+
+    return dp[0][0];
+  }
+};
+
+struct Path1 {
   int his; // min history hp
   int cur; // current hp
 };
 
-struct Room {
-  Path pmaxh; // path with max history hp
-  Path pmaxc; // path with max current hp
+struct Room1 {
+  Path1 pmaxh; // path with max history hp
+  Path1 pmaxc; // path with max current hp
 };
 
-class Solution { // 2ms
+class Solution1 { // 2ms
 public:
   int calculateMinimumHP(vector<vector<int>> &dungeon) {
     // If start with 0
@@ -24,7 +62,7 @@ public:
     //      path 2: 2 -> -1 -> -2 -> -3 ===> min health: -3
     // choose path 1 -> return -(-5) + 1 = 6
 
-    vector<vector<Room>> map(dungeon.size(), vector<Room>(dungeon[0].size()));
+    vector<vector<Room1>> map(dungeon.size(), vector<Room1>(dungeon[0].size()));
 
     int val = dungeon[0][0];
     map[0][0] = {{val, val}, {val, val}};
@@ -54,14 +92,14 @@ public:
   }
 
 private:
-  void nextPath(const int val, Path* new_p, const Path* prev_p) {
+  void nextPath(const int val, Path1* new_p, const Path1* prev_p) {
     new_p->cur = prev_p->cur + val;
     new_p->his = min(new_p->cur, prev_p->his);
   }
 
-  void nextRoom(const int val, Room* next, const Room* prev) {
+  void nextRoom(const int val, Room1* next, const Room1* prev) {
     // one previous room
-    Path path1, path2;
+    Path1 path1, path2;
     nextPath(val, &path1, &prev->pmaxc);
     nextPath(val, &path2, &prev->pmaxh);
 
@@ -69,7 +107,7 @@ private:
     next->pmaxh = path1.his > path2.his ? path1 : path2;
   }
 
-  void renewRoom(Room* orig, const Room* new_) {
+  void renewRoom(Room1* orig, const Room1* new_) {
     if (orig->pmaxc.cur == new_->pmaxc.cur) {
       // don't change pmaxc.cur, only change pmaxc.his
       orig->pmaxc.his = max(orig->pmaxc.his, new_->pmaxc.his);
@@ -84,11 +122,11 @@ private:
     } else {}
   }
 
-  void nextRoom(const int val, Room* next, const Room* prev1, const Room* prev2) {
+  void nextRoom(const int val, Room1* next, const Room1* prev1, const Room1* prev2) {
     // two previous rooms
     nextRoom(val, next, prev1);
 
-    Room tmp_room;
+    Room1 tmp_room;
     nextRoom(val, &tmp_room, prev2);
     renewRoom(next, &tmp_room);
   }
@@ -137,8 +175,8 @@ private:
 int main() {
   Solution demo;
   vector<vector<int>> dungeon{
-      {3,3,3},
-      {5,10,1},
-      {10,30,5}};
+      {-2,-3,3},
+      {-5,-10,1},
+      {10,30,-5}};
   std::cout << demo.calculateMinimumHP(dungeon) << "\n";
 }
