5017eb98030bbf1409605d890e5b6b9fe8a21c98
iktCalT <zzj610644618@live.com>
2026-07-19 18:16:38 +0800

    3954

---
+++ Jul-2026/3954-SumOfCompatibleNumbersInRangeI.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int sumOfGoodIntegers(int n, int k) {
+    int sum = 0;
+    for (int i = max(1, n - k); i <= n + k; ++i) {
+      sum += (n & i) == 0 ? i : 0;
+    }
+    return sum;
+  }
+};
